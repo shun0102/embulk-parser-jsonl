67c959e293d61a094ed6c0749a116f66673fd237
Muga Nishizawa <muga.nishizawa@gmail.com>
2016-02-14 00:00:56 -0800

    Fix guess/jsonl.rb: use SchemaGuess.from_hash_records to guess columns option

--- lib/embulk/guess/jsonl.rb
+++ lib/embulk/guess/jsonl.rb
@@ -1,84 +1,33 @@
 require 'json'
+require "embulk/parser/jsonl.rb"
 
 module Embulk
   module Guess
+    # $ embulk guess -g "jsonl" partial-config.yml
 
-    # TODO implement guess plugin to make this command work:
-    #      $ embulk guess -g "jsonl" partial-config.yml
-    #
-    #      Depending on the file format the plugin uses, you can use choose
-    #      one of binary guess (GuessPlugin), text guess (TextGuessPlugin),
-    #      or line guess (LineGuessPlugin).
-
-    require "embulk/parser/jsonl.rb"
-
-    #class JsonlParserGuessPlugin < GuessPlugin
-    #  Plugin.register_guess("jsonl", self)
-    #
-    #  def guess(config, sample_buffer)
-    #    if sample_buffer[0,2] == GZIP_HEADER
-    #      guessed = {}
-    #      guessed["type"] = "jsonl"
-    #      guessed["property1"] = "guessed-value"
-    #      return {"parser" => guessed}
-    #    else
-    #      return {}
-    #    end
-    #  end
-    #end
-
-    #class JsonlParserGuessPlugin < TextGuessPlugin
-    #  Plugin.register_guess("jsonl", self)
-    #
-    #  def guess_text(config, sample_text)
-    #    js = JSON.parse(sample_text) rescue nil
-    #    if js && js["mykeyword"] == "keyword"
-    #      guessed = {}
-    #      guessed["type"] = "jsonl"
-    #      guessed["property1"] = "guessed-value"
-    #      return {"parser" => guessed}
-    #    else
-    #      return {}
-    #    end
-    #  end
-    #end
-
-    class JsonlParserGuessPlugin < LineGuessPlugin
+    class Jsonl < LineGuessPlugin # TODO should use GuessPlugin instead of LineGuessPlugin
       Plugin.register_guess("jsonl", self)
 
       def guess_lines(config, sample_lines)
+        #return {} unless config.fetch("parser", {}).fetch("type", "jsonl") == "jsonl"
+
+        rows = []
+
         columns = {}
         sample_lines.each do |line|
-          hash = JSON.parse(line)
-          hash.each do |k, v|
-            columns[k] = get_embulk_type(v)
-          end
+          rows << JSON.parse(line)
         end
-        schema = []
-        columns.each do |k,v|
-          schema << {'name' => k, 'type' => v}
-        end
-        guessed = {}
-        guessed["type"] = "jsonl"
-        guessed["columns"] = schema
-        return {"parser" => guessed}
-      end
 
-      private
+        return {} if rows.size <= 3
 
-      def get_embulk_type(val)
-        case val
-        when TrueClass
-          return "boolean"
-        when FalseClass
-          return "boolean"
-        when Integer
-          return "long"
-        when Float
-          return "double"
-        else
-          return "string"
+        columns = Embulk::Guess::SchemaGuess.from_hash_records(rows).map do |c|
+          column = {name: c.name, type: c.type}
+          column[:format] = c.format if c.format
+          column
         end
+        parser_guessed = {"type" => "jsonl"}
+        parser_guessed["columns"] = columns
+        return {"parser" => parser_guessed}
       end
     end
   end
